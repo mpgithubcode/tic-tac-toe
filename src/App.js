e94565f5f909f7169d2373a824541e2084cf1c0e
mpgithubcode <github@jobs.slmail.me>
2023-01-31 05:31:10 -0500

    moved functions to constants file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import { useState } from "react";
+import { grid, calculateWinner } from "./constants";
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
 
 export function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>
 };
 
-export function Board({ xIsNext, squares, onPlay, uStatus }) {
-  const grid = [0,1,2,3,4,5,6,7,8]
+export function Board({ xIsNext, squares, onPlay }) {
 
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
@@ -38,31 +19,29 @@ export function Board({ xIsNext, squares, onPlay, uStatus }) {
       nextSquares[i] = "O";
     }
     onPlay(nextSquares);
-  }
+  };
 
     const winner = calculateWinner(squares);
     let status;
     if (winner) {
       status = "Winner: " + winner;
-      uStatus("Start")
     } else {
       status = "Next player: " + (xIsNext ? "X" : "O");
-      uStatus("Reset")
-    }
+    };
+
   return <>
     <div className="status">{status}</div>
-    <div className="board"> 
-      {grid.map( i => (
-        <Square className="square" value={squares[i]} onSquareClick={() => handleClick(i)} />
-      ))}
-    </div>
+      <div className="board"> 
+        {grid.map( i => (
+          <Square className="square" value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))};
+      </div>
     </>
 }
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const [status, setStatus] = useState("Start");
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -70,22 +49,20 @@ export default function Game() {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-  }
+  };
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-  }
+  };
 
-  function handleStatus(gameStatus) {
-    setStatus(gameStatus);
-  }
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = `${move} ${squares}`;
+      description = `go to ${move}`;
     } else {
       description = 'Go to game start';
-    }
+    };
+
   return (
     <li key={move}>
       <button onClick={() => jumpTo(move)}>{description}</button>
@@ -97,13 +74,13 @@ export default function Game() {
     <div className="game">
       <div className="container">
         <div className="game-board">
-          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} uStatus={handleStatus} />
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
         </div>
         <div className="game-info">
           <ol>{moves}</ol>
         </div>
       </div>
-      <button>{status} Game</button>
+      <button>Game</button>
     </div>
   );
-} 
+};
